5edee7afc2301ad31a204bfaa84cee5159a9afe3
GOOHAESEUNG <amu6675@naver.com>
2023-07-12 01:17:04 +0900

    ㄹfeat : 게시판 카테고리 선택 기능, 전ì체 ã조회, 상세조회

--- post/models.py
+++ post/models.py
@@ -2,16 +2,17 @@ from django.db import models
 from django.utils import timezone
 from enum import Enum
 
+# 카테고리
 class category(Enum):
-    위험 = '대기'
-    추행 = '취소'
-    폭행 = '완료'
+    위험 = '위험'
+    추행 = '추행'
+    폭행 = '폭행'
     침입 = '침입'
 
-# Create your models here.
+# 글 생성
 class Post(models.Model):
     title = models.CharField(max_length=50)
   #  author = models.ForeignKey(User, on_delete=models.CASCADE)
-    date = models.DateField(default=timezone.now())
+    datetime = models.DateTimeField(auto_now=True)
     body = models.CharField(max_length=200)
     category = models.CharField(max_length=20, choices=[(status.value, status.name) for status in category])
--- post/views.py
+++ post/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Post
 
-# Create your views here.
+
+def create(request):
+    if request.method == 'POST':
+        post = Post()
+        post.title = request.POST['title']
+        post.body = request.POST['body']
+        post.category = request.POST['category']
+        post.save()
+        return redirect("create")
+
+    return render(request, "html/post.html")
+
+
+def post_list(request):
+    # 모든 게시글 가져오기
+    posts = Post.objects.all()
+
+    context = {
+        'posts': posts,
+    }
+
+    return render(request, 'html/post_list.html', context)
+
+def post_detail(request, post_id):
+    # 게시글 가져오기
+    post = get_object_or_404(Post, pk=post_id)
+
+    context = {
+        'post': post,
+    }
+
+    return render(request, 'html/post_detail.html', context)
